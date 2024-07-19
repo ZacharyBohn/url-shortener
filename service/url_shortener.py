102f74440c0966eb82158c686dd3524654c74f06
Zachary Bohn <zacharybohn95@gmail.com>
2024-07-19 11:52:33 -0400

    More URL schemes

--- service/url_shortener.py
+++ service/url_shortener.py
@@ -8,6 +8,24 @@ class InvalidUrlException(Exception): pass
 class UrlScheme(Enum):
     HTTP = "http"
     HTTPS = "https"
+    FTP = "ftp"
+    MAILTO = "mailto"
+    FILE = "file"
+    TEL = "tel"
+    NEWS = "news"
+    IRC = "irc"
+    RTSP = "rtsp"
+    SPOTIFY = "spotify"
+    SFTP = "sftp"
+    SSH = "ssh"
+    GIT = "git"
+    SVN = "svn"
+    MAGNET = "magnet"
+    ED2K = "ed2k"
+    AFP = "afp"
+    NFS = "nfs"
+    LDAP = "ldap"
+    BITCOIN = "bitcoin"
 
 def generate_short_url(
   url: str,
