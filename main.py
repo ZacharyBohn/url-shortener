373a9923a59ed82e37492a543c57b39c1b329e6e
Zachary Bohn <zacharybohn95@gmail.com>
2024-07-20 11:14:02 -0400

    Fastapi methods

--- main.py
+++ main.py
@@ -7,7 +7,14 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/list_urls")
+async def list_urls():
+  return {"message": "ok"}
+  
+@app.get("/redirect")
+async def redirect(short_url: str):
+  return {"message": "ok"}
+  
+@app.post("/shorten_url")
+async def shorten_url(url: str, short_url: str | None):
+  return {"message": "ok"}
