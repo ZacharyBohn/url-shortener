b50fde4abb4689d2ae05d4d8dabaa82393e54a87
Zachary Bohn <zacharybohn95@gmail.com>
2024-07-20 11:44:56 -0400

    Placeholder test files

---
+++ tests/test_apis.py
@@ -0,0 +1,6 @@
+import unittest
+
+class TestApis(unittest.TestCase):
+  def test_some_function(self):
+    self.assertTrue(True)
+    return
---
+++ tests/test_db.py
@@ -0,0 +1,6 @@
+import unittest
+
+class TestDb(unittest.TestCase):
+  def test_some_function(self):
+    self.assertTrue(True)
+    return
