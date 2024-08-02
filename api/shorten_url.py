484de0050a98f04992680df894abccbbc26562e2
Zachary Bohn <zacharybohn95@gmail.com>
2024-08-01 23:40:10 -0400

    Shorten url service

--- api/shorten_url.py
+++ api/shorten_url.py
@@ -1,7 +1,29 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Response
+
+from service.shorten_url import InvalidUrlException, generate_short_url
+from schemas.short_url_response import ShortUrlResponse
+from schemas.error_response import ErrorReponse
 
 router = APIRouter()
 
 @router.get("/shorten_url")
-async def list_urls(url: str, short_url: str | None):
-  return {"message": "ok"}
+async def list_urls(url: str, short_url: str | None, response: Response):
+	if short_url == None:
+		try:
+			generated_short_url: str = generate_short_url(
+				url=url,
+			)
+			return ShortUrlResponse(
+				short_url=generated_short_url,
+				original_url=url,
+			)
+		except InvalidUrlException:
+			# 400 =
+			# the client sent an incorrect request
+			response.status_code = 400
+			return ErrorReponse(error='Invalid custom URL')
+		except:
+			# 500 =
+			# internal server error
+			response.status_code = 500
+			return ErrorReponse(error='Internal server error')
