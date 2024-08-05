18e7a7c3d03ed48e35edcd8d043642c0739cb4e7
Zachary Bohn <zacharybohn95@gmail.com>
2024-08-05 16:44:32 -0400

    Reorganizes main file

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from api import list_urls
 from api import redirect
 from api import shorten_url
 
-app = FastAPI()
-
 # TODO: fill in with the actual domain
 domain: str = 'domain.com'
 short_id_length: int = 6
+
+app = FastAPI()
 
 app.include_router(list_urls.router)
 app.include_router(redirect.router)
