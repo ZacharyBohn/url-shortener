d48b2842d4acf3ea4739c494d4d4c3296823cf6c
Zachary Bohn <zacharybohn95@gmail.com>
2024-08-05 23:19:01 -0400

    Switches to uuidv4

--- utils/utilities.py
+++ utils/utilities.py
@@ -1,6 +1,5 @@
-import random
-import string
 import hashlib
+from uuid import uuid4
 from pydantic import PositiveInt
 
 from ..interfaces.utilities import IUtilities
@@ -10,10 +9,7 @@ class Utilities(IUtilities):
 	def generate_random_string(self, length: int) -> str:
 		if length < 1:
 			return ''
-		# alphanumeric, both upper and lower case
-		characters = string.ascii_letters + string.digits
-		random_string = ''.join(random.choice(characters) for _ in range(length))
-		return random_string
+		return str(uuid4())[:length]
 
 	def hash_string(self, string: str, output_length: PositiveInt) -> str:
 		hash_bytes: bytes = hashlib.sha256(string.encode()).digest()
